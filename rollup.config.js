2fb1517e12dc0318c5e7eb4b794f859677c34c00
Jtree03 <wowns0903@gmail.com>
2022-05-20 15:39:42 +0900

    Add: init settings

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      strict: false,
+    },
+  ],
+  plugins: [resolve({ extensions: ['.tsx', '.ts'] }), typescript({ objectHashIgnoreUnknownHack: false })],
+  external: ['react', '@emotion/styled'],
+};
