8bbe599c822be7a71250c88f886ced17a9b0f92b
조재준 <wowns0903@gmail.com>
2022-06-03 09:35:48 +0900

    Add: useSelection (#4)
    * Add: useSelection
    
    * Fix: useSelection event name

---
+++ src/useSelection.ts
@@ -0,0 +1,29 @@
+import React from 'react';
+import { SELECTION_RANGE } from './const';
+
+function useSelection() {
+  const [range, setRange] = React.useState<Range | null>(null);
+  const [isSelectRange, setIsSelectRange] = React.useState(false);
+
+  React.useEffect(() => {
+    const selectionChangeCallback = () => {
+      const selection = window.getSelection();
+
+      if (selection?.type === SELECTION_RANGE) {
+        setRange(selection.getRangeAt(0));
+        setIsSelectRange(true);
+      } else {
+        setRange(null);
+        setIsSelectRange(false);
+      }
+    };
+    document.addEventListener('selectionchange', selectionChangeCallback);
+    return () => {
+      document.removeEventListener('selectionchange', selectionChangeCallback);
+    };
+  }, []);
+
+  return { range, isSelectRange };
+}
+
+export default useSelection;
