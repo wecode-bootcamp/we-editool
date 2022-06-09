23ccf4a87f93bbb0043578a7c5f20a8c4ca0d456
Jtree03 <wowns0903@gmail.com>
2022-06-09 19:09:23 +0900

    Add: test automation with github action

--- __tests__/emotion.ts
+++ __tests__/emotion.ts
@@ -1,3 +1,18 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
 describe('library: emotion', () => {
-  test('', () => {});
+  test('css template test', () => {
+    const cssTemplate = css`
+      display: flex;
+    `;
+    expect(cssTemplate).toMatchSnapshot();
+  });
+
+  test(`styled test`, () => {
+    const StyledDiv = styled.div`
+      display: flex;
+    `;
+    expect(StyledDiv).toMatchSnapshot();
+  });
 });
--- __tests__/react-icons.tsx
+++ __tests__/react-icons.tsx
@@ -1,3 +1,25 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { BiBold, BiUnderline, BiItalic, BiLink } from 'react-icons/bi';
+
 describe('library: react-icons', () => {
-  test('', () => {});
+  test('render BiBold', () => {
+    const result = render(<BiBold size="20" />);
+    expect(result).toMatchSnapshot();
+  });
+
+  test('render BiUnderline', () => {
+    const result = render(<BiUnderline size="20" />);
+    expect(result).toMatchSnapshot();
+  });
+
+  test('render BiItalic', () => {
+    const result = render(<BiItalic size="20" />);
+    expect(result).toMatchSnapshot();
+  });
+
+  test('render BiLink', () => {
+    const result = render(<BiLink size="20" />);
+    expect(result).toMatchSnapshot();
+  });
 });
